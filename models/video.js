d8f1b2422e4a952b6fb9e9bb4030227c663b5306
MAC <mac@ara.local>
2020-05-23 15:28:30 +0700

    :card_file_box: video column name changed

--- models/video.js
+++ models/video.js
@@ -3,8 +3,8 @@ module.exports = (sequelize, DataTypes) => {
     'Video',
     {
       url: DataTypes.STRING,
-      title: DataTypes.STRING,
-      duration: DataTypes.STRING,
+      youtubeTitle: DataTypes.STRING,
+      youtubeTime: DataTypes.STRING,
     },
     {
       hooks: {
