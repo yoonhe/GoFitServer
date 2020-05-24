acd4916cf93d9ecfdb57078893ec6c77bcae63cf
MAC <mac@ara.local>
2020-05-24 13:19:27 +0700

    :triangular_flag_on_post: routes_daylog completed

--- routes/daylog.js
+++ routes/daylog.js
@@ -12,17 +12,20 @@ const db = require('../models');
 // 현재 날짜가 속한 달의 게시물을 전체 get
 router.get('/daylog', async (req, res, next) => {
   const { id } = req.user; // DB 의 userId임
-  const currentDate = moment().format('YYYY-MM');
+  const year = moment().format('YYYY');
+  const month = moment().format('MM');
   try {
     const logs = await db.Daylog.findAll({
       where: {
         userId: id,
         createdAt: {
-          [Op.gte]: new Date(`${currentDate}-01 00:00:00.000Z`),
-          [Op.lte]: new Date(`${currentDate}-31 23:59:59.000Z`),
+          [Op.gte]: new Date(`${year}-${month}-01 00:00:00.000Z`),
+          [Op.lte]: new Date(
+            `${year}-${month}-${new Date(year, month, 0).getDate()} 23:59:59.000Z`,
+          ),
         },
       },
-      include: [{ model: db.Healthlog }, { model: db.Video }],
+      include: [{ model: db.Video }, { model: db.Healthlog }],
       raw: true,
     });
 
@@ -38,16 +41,20 @@ router.get('/daylog', async (req, res, next) => {
 router.get('/daylog/:date', async (req, res, next) => {
   const { id } = req.user; // DB 의 userId임
   const targetDate = moment(req.params.date).format('YYYY-MM');
+  const year = moment(targetDate).format('YYYY');
+  const month = moment(targetDate).format('MM');
   try {
     const logs = await db.Daylog.findAll({
       where: {
         userId: id,
         createdAt: {
-          [Op.gte]: new Date(`${targetDate}-01 00:00:00.000Z`),
-          [Op.lte]: new Date(`${targetDate}-31 23:59:59.000Z`),
+          [Op.gte]: new Date(`${year}-${month}-01 00:00:00.000Z`),
+          [Op.lte]: new Date(
+            `${year}-${month}-${new Date(year, month, 0).getDate()} 23:59:59.000Z`,
+          ),
         },
       },
-      include: [{ model: db.Healthlog }, { model: db.Video }],
+      include: [{ model: db.Video }, { model: db.Healthlog }],
       raw: true,
     });
     res.send(200, logs);
@@ -76,6 +83,7 @@ router.post('/daylog', async (req, res, next) => {
         water,
       });
     });
+    res.status(200).send('create success!');
   } catch (e) {
     console.error(e);
     res.status(500).status('network error');
@@ -91,11 +99,12 @@ router.post('/daylog/edit/:targetId', async (req, res, next) => {
   try {
     await db.Daylog.update({ message }, { where: { id: targetId, UserId: id } });
     await db.Healthlog.update({ weight, water }, { where: { DaylogId: targetId } });
+    res.status(200).send('edit success!');
   } catch (e) {
     console.error(e);
     res.status(500).status('network error');
     next(e);
   }
-}); // 과거 포스트 수정
+});
 
 module.exports = router;
