21c008016d9c8dd55a325ee771f1589bb04dbf28
talentceffort <talentceffort@gmail.com>
2020-05-28 21:03:09 +0900

    Rank_Query_ing

--- seeders/20200517100252-userData.js
+++ seeders/20200517100252-userData.js
@@ -1,15 +1,16 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-await-in-loop */
+const bcrypt = require('bcrypt');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     const datas = [];
-    for (let i = 1; i < 6; i++) {
+    for (let i = 1; i < 15; i++) {
       const obj = {
         id: i,
         email: `test${i}@example.com`,
         username: `testUser${i}`,
-        password: `1234${i}`,
+        password: `1234`,
         height: `17${i}`,
         weight: 70 + i * 4,
         createdAt: new Date(),
