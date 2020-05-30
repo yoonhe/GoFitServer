b57acb8cf6ac4e8aee7839b038b483f59fa811d9
talentceffort <talentceffort@gmail.com>
2020-05-30 22:01:09 +0900

    Add seeders data

--- seeders/20200517100319-daylogData.js
+++ seeders/20200517100319-daylogData.js
@@ -1,16 +1,18 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-await-in-loop */
+const moment = require('moment');
 const models = require('../models');
 
 const { User } = models;
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const datas = ['', 'Good job today', 'excited', 'so tired', 'zzz...', 'okay'];
+    const datas = ['Hello Hello', 'Good job today', 'excited', 'so tired', 'zzz...', 'okay'];
     const results = [];
     for (let i = 1; i < 200; i++) {
       const oneUser = await User.findOne({ where: { id: Math.floor(Math.random() * 10) + 1 } });
 
+      // 이번 주 데이터
       const obj = {
         id: i,
         message: datas[Math.floor(Math.random() * datas.length)],
@@ -18,7 +20,17 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       };
+
+      // 일주일 전
+      const obj2 = {
+        id: 199 + i,
+        message: datas[Math.floor(Math.random() * datas.length)],
+        UserId: oneUser.id,
+        createdAt: moment().subtract(7, 'd').format('YYYY-MM-DD HH:mm:ss'),
+        updatedAt: moment().subtract(7, 'd').format('YYYY-MM-DD HH:mm:ss'),
+      };
       results.push(obj);
+      results.push(obj2);
     }
     return queryInterface.bulkInsert('Daylogs', results, {});
   },
--- seeders/20200517100340-healthlogData.js
+++ seeders/20200517100340-healthlogData.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-await-in-loop */
+const moment = require('moment');
 const models = require('../models');
 
 const { Daylog } = models;
@@ -7,17 +8,28 @@ const { Daylog } = models;
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const results = [];
-    for (let i = 1; i < 6; i++) {
+    for (let i = 1; i < 200; i++) {
       const daylog = await Daylog.findOne({ where: { id: i } });
       const obj = {
         id: i,
-        weight: 50 + i * 4,
-        water: i - 1,
+        weight: 50 + Math.floor(Math.random() * 9),
+        water: Math.floor(Math.random() * 10),
         daylogId: daylog.id,
         createdAt: new Date(),
         updatedAt: new Date(),
       };
+
+      const obj2 = {
+        id: 199 + i,
+        weight: 50 + Math.floor(Math.random() * 9),
+        water: Math.floor(Math.random() * 10),
+        daylogId: daylog.id,
+        createdAt: moment().subtract(7, 'd').format('YYYY-MM-DD HH:mm:ss'),
+        updatedAt: moment().subtract(7, 'd').format('YYYY-MM-DD HH:mm:ss'),
+      };
+
       results.push(obj);
+      results.push(obj2);
     }
     return queryInterface.bulkInsert('Healthlogs', results, {});
   },
