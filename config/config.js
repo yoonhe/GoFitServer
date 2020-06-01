7a5d55ab21062ab5a58052b19191d969ab957325
MAC <mac@ara.local>
2020-06-01 22:59:00 +0700

    :bug: fix timezone

--- config/config.js
+++ config/config.js
@@ -8,6 +8,15 @@ module.exports = {
     host: process.env.DEV_HOST,
     port: process.env.DEV_PORT,
     dialect: 'mysql',
+    timezone: '+09:00',
+    dialectOptions: {
+      charset: 'utf8mb4',
+      dateStrings: true,
+      typeCast: true,
+    },
+    define: {
+      timestamps: true,
+    },
   },
   test: {
     username: process.env.TEST_USERNAME,
@@ -16,6 +25,15 @@ module.exports = {
     host: process.env.TEST_HOST,
     port: process.env.TEST_PORT,
     dialect: 'mysql',
+    timezone: '+09:00',
+    dialectOptions: {
+      charset: 'utf8mb4',
+      dateStrings: true,
+      typeCast: true,
+    },
+    define: {
+      timestamps: true,
+    },
   },
   production: {
     username: process.env.PROD_USERNAME,
@@ -24,5 +42,14 @@ module.exports = {
     host: process.env.PROD_HOST,
     port: process.env.PROD_PORT,
     dialect: 'mysql',
+    timezone: '+09:00',
+    dialectOptions: {
+      charset: 'utf8mb4',
+      dateStrings: true,
+      typeCast: true,
+    },
+    define: {
+      timestamps: true,
+    },
   },
 };
